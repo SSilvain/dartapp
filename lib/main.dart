f0af16e6a3441aa3e796d420982db7c379ec6147
silvain_mint <silver383@gmail.com>
2021-07-17 18:24:31 +0300

    body

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
-// import 'package:flutter/material.dart';
-import 'myclass.dart';
+import 'package:flutter/material.dart';
+// import 'myclass.dart';
 
 void main() {
-  var myFirstObject = MyClass()
-    ..firstName = "Serhii"
-    ..surName = "Yuzhytsa";
-  print("${myFirstObject.firstName} ${myFirstObject.surName}");
-
-  int zzz;
-  zzz = 7;
-
-  print(zzz);
+  runApp(MyApp());
 }
 
-String myFunc() {
-  return 'from myFunc';
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.brown),
+      home: Scaffold(
+        appBar: AppBar(title: Text('AppBarMyBar'), centerTitle: true),
+        body: Center(
+            child: Text(
+          "BodyText",
+          style:
+              TextStyle(fontSize: 40, color: Colors.blue, fontFamily: 'Qahiri'),
+        )),
+        floatingActionButton: FloatingActionButton(
+          child: Text('put'),
+          onPressed: () {
+            print('put');
+          },
+        ),
+      ),
+    );
+  }
 }
